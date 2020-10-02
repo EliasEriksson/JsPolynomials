dff52332fe626240bc35d9758c521b51b1133027
EliasEriksson <EliasEriksson95@gmail.com>
2020-10-02 13:31:05 +0200

    bug fixing term formatting

--- polynomial.js
+++ polynomial.js
@@ -1,33 +1,81 @@
 class Term {
+    /**
+     * represents a mathematical term a * x ^ b
+     * where x is given value in format or evaluate method.
+     * evaluate requires x to be a numeric value.
+     */
     constructor(multiple, exponent) {
+        /**
+         * (multiple * x ^ exponent)
+         */
         this.multiple = multiple;
         this.exponent = exponent;
     }
 
     format(x) {
+        /**
+         * string representation of the term
+         */
         let result = "";
         if (x && this.multiple) {
             if (this.exponent === 0) {
                 return this.multiple;
             }
             if (this.multiple !== 1) {
-                result += `${this.multiple} * `
+                result += `${this.multiple} * `;
             }
             if (this.exponent === 1) {
                 if (result) {
-                    result += `${x}`
+                    result += `${x}`;
                 } else {
-                    return `${x}`
+                    return `${x}`;
                 }
             } else {
                 result += `${x} ^ ${this.exponent}`;
             }
-            result = `(${result})`
+            result = `(${result})`;
         }
         return result;
     }
 
+
+
+    form(x) {
+        // 3 1 1 1
+        // (3 * -1 ^ 3) + (-1 ^ 2) + (1 * 0 ^ 1) + -1
+
+        // VL om m != 0 och x != 0 och m != 1
+        // HL om m != 0 och x != 0 och e != 0
+        //      exp om HL och exp != 1
+
+        let result = "";
+        let res = []
+        if (this.multiple) {
+            if (x){
+                if (this.multiple !== 1) {
+                    // VL om m != 0 och x != 0 och m != 1
+                    res.push(`${this.multiple}`)
+                }
+                if (this.exponent) {
+                    // HL om m != 0 och x != 0 och e != 0
+                    if (this.exponent !== 1) {
+                        // exp om HL och exp != 1
+                        res.push(`${x} ^ ${this.exponent}`)
+                    }
+                } else {
+                    return x;
+                }
+            } else if (!this.exponent) {
+                return this.multiple;
+            }
+        }
+        return res ? `(${res.join(" * ")})` : "";
+    }
+
     evaluate(x) {
+        /**
+         * calculates the value of the term based on the value of given x
+         */
         return this.multiple * x ** this.exponent;
     }
 }
@@ -58,21 +106,36 @@ class Polynomial {
     }
 
     format(x = "x", evaluate = true) {
+        /**
+         * string representation of the terms in the polynomial
+         *
+         * x is allowed to be the string "x" or any number
+         * if x is a number and evaluate is false the polynomial will only
+         * replace "x" with the value but not calculate it.
+         */
         return `f(${x}) = ` + this.terms.map(
-            term => term.format(x)
-        ).filter(term => term).join(" + ") + (x !== "x" && evaluate ? " = " + this.evaluate(x) : "");
+            term => term.form(x)
+        ).filter(
+            term => term
+        ).join(" + ") + (evaluate && (evaluate = this.evaluate(x)) ? " = " + evaluate : "");
     }
 
     evaluate(x) {
+        /**
+         * evaluates all the terms in the polynomial with the value of x
+         */
         let sum = 0;
         for (let i = 0; i < this.terms.length; i++) {
             sum += this.terms[i].evaluate(x);
         }
+        console.log(sum)
         return sum;
     }
 }
 
 
-polynome = new Polynomial(1, 0, 0);
-console.log(polynome.format())
-console.log(polynome.format(5, true))
+polynome = new Polynomial(3, 1, 1, 1);
+
+for (let x = -5; x < 5; x++) {
+    console.log(polynome.format(x));
+}
